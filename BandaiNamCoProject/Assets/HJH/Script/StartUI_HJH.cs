5fbbd7e847327b874ffd44c41e96833a09d59668
JinHo Hong <ghddhksduq@naver.com>
2023-09-17 19:59:13 +0900

    로딩 메니저 + 스타트 씬

---
+++ BandaiNamCoProject/Assets/HJH/Script/StartUI_HJH.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartUI_HJH : MonoBehaviour
+{
+    public GameObject quitPopUp;
+    public AudioSource audio;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(quitPopUp != null)
+            {
+                quitPopUp.SetActive(true);
+            }
+        }
+    }
+
+    public void StartButton()
+    {
+        if(audio != null)
+        {
+            audio.Play();
+        }
+        Invoke("MoveScene", 0.01f);
+    }
+    public void MoveScene()
+    {
+        LoadingManager_HJH.LoadScene("GameScene");
+    }
+    public void QuitApp()
+    {
+        Application.Quit();
+    }
+}
