ff1ab16e1aa24be629395e6c682cb1a5d4230dd5
JinHo Hong <ghddhksduq@naver.com>
2023-09-24 12:41:05 +0900

    Revert "error해결"
    This reverts commit 93ac4ff15d73f60d74a88e348f0f74f4874b4bb3.

--- BandaiNamCoProject/Assets/LSW/Scripts/CharacterMovement2D_LSW.cs
+++ BandaiNamCoProject/Assets/LSW/Scripts/CharacterMovement2D_LSW.cs
@@ -10,119 +10,55 @@ public class CharacterMovement2D_LSW : MonoBehaviour
         Idle,
         Move,
         Fly,
-        Die,
     }
-<<<<<<< Updated upstream
     public State state;
     // 각각 이동속도와 점프힘
     public float moveSpeed = 100.0f;
     public float flyForce = 200.0f;
     
-=======
-    private State state = State.Idle;
-
-    // 각각 이동속도와 점프힘
-    [SerializeField]
-    private float moveSpeed = 50.0f;
-    [SerializeField]
-    private float flyForce = 200.0f;
-
->>>>>>> Stashed changes
     private Rigidbody2D rb;
-
+    
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-<<<<<<< Updated upstream
         state = State.Idle;
         Debug.Log(transform.up);
-=======
-
-
->>>>>>> Stashed changes
     }
 
     private void FixedUpdate()
     {
-        if (state == State.Move)
+       if (state == State.Move)
         {
-<<<<<<< Updated upstream
             rb.AddForce(transform.up * moveSpeed);
             gameObject.transform.up = rb.velocity;
-=======
-            rb.velocity = new Vector2(moveSpeed, 0);
->>>>>>> Stashed changes
         }
     }
 
     void Update()
     {
-<<<<<<< Updated upstream
         if(state == State.Idle) 
         {
             rb.velocity = Vector2.zero;
         }
         // space 바를 누르면 이제 스테이트마다 하는 행동 실시
         if (Input.GetKeyDown(KeyCode.Space))
-=======
-        switch (state)
->>>>>>> Stashed changes
         {
-            case State.Idle:
-                UpdateIdle();
-                break;
-            case State.Move:
-                UpdateMove();
-                break;
-            case State.Fly:
-                UpdateFly();
-                // 땅에 닿았을 때 죽는 상태로
-                if (rb.velocity.magnitude > flyForce)
-                {
-                    state = State.Die;
-                }
-                break;
-            case State.Die:
-                // 다시 처음스폰장소 / 아니면 게임오버창으로
-                break;
+            switch (state)
+            {
+                case State.Idle:
+                    state = State.Move;
+                    break;
+                case State.Move:
+                    break;
+                case State.Fly:
+                    rb.AddForce(Vector2.up * flyForce, ForceMode2D.Impulse);
+                    break;
+            }
         }
     }
 
-<<<<<<< Updated upstream
     private void OnTriggerEnter(Collider other) //이게 Collider Tag이용해서 Trigger를 노린건데 안됨 그래서 다시 해보고 있음.
-=======
-    private void UpdateIdle()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            state = State.Move;
-        }
-    }
-
-    private void UpdateMove()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            state = State.Fly;
-        }
-    }
-
-    private void UpdateFly()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            rb.AddForce(Vector2.up * flyForce, ForceMode2D.Impulse);
-        }
-    }
-
-
-}
-
-
-    /*
-    private void startTriggerEnter(Collider2D other) //이게 Collider Tag이용해서 Trigger를 노린건데 안됨 그래서 다시 해보고 있음.
->>>>>>> Stashed changes
     {
         if (state == State.Move && other.CompareTag("StartPoint"))
         {
@@ -130,5 +66,8 @@ public class CharacterMovement2D_LSW : MonoBehaviour
         }
             
     }
-    */
+
     
+
+
+}
