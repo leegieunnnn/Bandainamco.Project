56362dd1568f0ce05f3ae1d5022cf87e957ea12f
JinHo Hong <ghddhksduq@naver.com>
2023-10-06 22:23:32 +0900

    누락

--- BandaiNamCoProject/Assets/HJH/Script/StartUI_HJH.cs
+++ BandaiNamCoProject/Assets/HJH/Script/StartUI_HJH.cs
@@ -27,7 +27,7 @@ public class StartUI_HJH : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape)&& Time.timeScale != 0f)
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             if (optionCanvas.activeInHierarchy)
             {
