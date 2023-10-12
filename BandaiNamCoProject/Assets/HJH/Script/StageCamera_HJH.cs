bd281f1a4cfab6ca143006a7277efe36bf2de827
JinHo Hong <110793489+mynameisjinhohong@users.noreply.github.com>
2023-10-12 16:32:15 +0900

    발걸음 제작

--- BandaiNamCoProject/Assets/HJH/Script/StageCamera_HJH.cs
+++ BandaiNamCoProject/Assets/HJH/Script/StageCamera_HJH.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class StageCamera_HJH : MonoBehaviour
@@ -6,6 +7,11 @@ public class StageCamera_HJH : MonoBehaviour
     public float startPoint;
     public float endPoint;
 
+    public float upPower;
+    public bool walk;
+    public float walkTime;
+    public float walkWaitTime;
+    float currentTime;
 
     public GameObject background;
     public Vector3 backgroundSize;
@@ -41,10 +47,38 @@ public class StageCamera_HJH : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float stageMove = Input.GetAxis("Horizontal");
-        gameObject.transform.position += new Vector3(stageMove * cameraMoverSpeed, 0, -10);
-        gameObject.transform.position = new Vector3(Mathf.Clamp(gameObject.transform.position.x, startPoint, endPoint), 0, 0);
+        float stageMove = Input.GetAxisRaw("Horizontal");
+        if (!walk && stageMove != 0)
+        {
+            StartCoroutine(Walk(stageMove));
+        }
+    }
+    IEnumerator Walk(float stageMove)
+    {
+        walk = true;
+        while (true)
+        {
+            currentTime += Time.deltaTime;
+            if(currentTime > walkTime)
+            {
+                break;
+            }
+            yield return null;
+            if(currentTime < walkTime / 2)
+            {
+                gameObject.transform.position += new Vector3(stageMove * cameraMoverSpeed, upPower, -10);
+            }
+            else
+            {
+                gameObject.transform.position += new Vector3(stageMove * cameraMoverSpeed, -upPower, -10);
+            }
+            gameObject.transform.position = new Vector3(Mathf.Clamp(gameObject.transform.position.x, startPoint, endPoint), gameObject.transform.position.y, -10);
+        }
+        yield return new WaitForSeconds(walkWaitTime);
         gameObject.transform.position = new Vector3(gameObject.transform.position.x, 0, -10);
+        currentTime = 0;
+        walk = false;
 
     }
+
 }
