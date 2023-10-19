f224a6c9fd878344f5876c85c714b01f4366dab6
이진호 <55572533+JinHoLeeee@users.noreply.github.com>
2023-10-19 17:30:15 +0900

    Bubble Shader 추가

--- BandaiNamCoProject/Assets/LJH/Scripts/WaveCollider_LJH.cs
+++ BandaiNamCoProject/Assets/LJH/Scripts/WaveCollider_LJH.cs
@@ -14,12 +14,12 @@ public class WaveCollider_LJH : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("Wave Collision : " + collision.collider.name);
 
         if (collision.collider.transform.CompareTag(TagStrings.PlayerTag))
         {
-            if (!parentWater.isFinished)
-                parentWater.SendFinishWave();
+            Debug.Log("Wave Collision : " + collision.collider.name);
+            //if (!parentWater.isFinished)
+            //   parentWater.SendFinishWave();
         }
     }
 }
