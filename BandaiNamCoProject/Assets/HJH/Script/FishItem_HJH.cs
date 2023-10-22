8346d5af4465121a7b6cb1d7f3093635a40e8867
JinHo Hong <ghddhksduq@naver.com>
2023-10-22 22:37:03 +0900

    물고기 작업 시작

---
+++ BandaiNamCoProject/Assets/HJH/Script/FishItem_HJH.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishItem_HJH : BaseItem_LJH
+{
+    public float fishTime;
+
+    public override void OnTriggerEnter2D(Collider2D other)
+    {
+
+        base.OnTriggerEnter2D(other);
+    }
+}
